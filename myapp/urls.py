e07a3424387a247d8ec1e84b3831075e205394d6
nikhildubey-23 <ravannick0089@gmail.com>
2025-01-29 21:23:53 +0530

    solving the probelm of static file

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -9,4 +8,4 @@ urlpatterns = [
     path('clear_history/', views.clear, name='clear_history'),
     path('about/', views.about, name='about'),
     path('reachout/', views.reachout, name='reachout'),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
