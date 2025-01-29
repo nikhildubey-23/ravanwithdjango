4728d60ec8a6a9c8d0d8ca354c1c9ff92b8be7d3
nikhildubey-23 <ravannick0089@gmail.com>
2025-01-29 20:55:33 +0530

    updating the urls  file to deployee

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('clear_history/', views.clear, name='clear_history'),
     path('about/', views.about, name='about'),
     path('reachout/', views.reachout, name='reachout'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
